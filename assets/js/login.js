ec85b81690e55539b1a50a13e32054eee091ce91
zhouwei <2951436411@qq.com>
2020-11-26 09:50:07 +0800

    仓库初始化

---
+++ assets/js/login.js
@@ -0,0 +1,13 @@
+$(function () {
+    $("#user").click(function () {
+        $(".logon").show();
+        $(".login").hide();
+    })
+    $("#logon").click(function () {
+        $(".login").show();
+        $(".logon").hide();
+    })
+    
+
+    
+  })
