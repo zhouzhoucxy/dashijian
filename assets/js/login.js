e4c5f49e73d75a69f1c115260527a012cc4ad2e1
zhouwei <2951436411@qq.com>
2020-11-26 16:50:34 +0800

    注册登录页面完成

--- assets/js/login.js
+++ assets/js/login.js
@@ -7,7 +7,60 @@ $(function () {
         $(".login").show();
         $(".logon").hide();
     })
-    
 
-    
-  })
+    //表单验证 
+    var form = layui.form;
+    var layer = layui.layer;
+   
+
+    form.verify({
+        pwd: [/^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'],
+        repassword: function (value) {
+            if (value !== $("input[name=password]").val()) {
+                return '两次密码不一致';
+            }
+        }
+    })
+
+    $(".logon").on("submit", function (e) {
+        e.preventDefault();
+        var username = $(".logon [name=username]").val();
+        var password = $(".logon [name=password]").val();
+        $.ajax({
+            type: "post",
+            url: "/api/reguser",
+            data: {
+                username,
+                password
+            },
+           
+            success: function (res) {
+                if (res.status !== 0) {
+                    layer.msg(res.message)
+                } else {
+                    layer.msg(res.message)
+                    $("#logon").click()
+                }
+            }
+        });
+
+    })
+
+    $("#logins").on("submit", function (e) {
+        e.preventDefault();
+        var forms = $(this).serialize();
+       
+      $.ajax({
+            type: "post",
+            url: "/api/login",
+            data:forms,
+            success: function (res) {
+                if (res.status !== 0) return layer.msg(res.message);
+                location.href = 'index.html';
+              }
+        }) 
+
+      })
+
+
+})
